0dc65cddd016346833bcc9c8af04a85745ccce1d
Joey Weijters <385237@student.fontys.nl>
2018-01-10 10:37:10 +0100

    Added Prompt Box

---
+++ ev5/PromptBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ev5
+{
+    public partial class PromptBox : Form
+    {
+        public PromptBox(string message, string buttonText = "Ok", string placeholder = "")
+        {
+            InitializeComponent();
+            label1.Text = message;
+            button1.Text = buttonText;
+            textBox1.Text = placeholder;
+            this.Show();
+            textBox1.Focus();
+        }
+
+        /// <summary>
+        /// Returns the text of the textbox when the button is pressed
+        /// </summary>
+        public event EventHandler<string> OnFinished;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            if (OnFinished != null)
+            {
+                OnFinished.Invoke(this, textBox1.Text);
+            }
+        }
+    }
+}
